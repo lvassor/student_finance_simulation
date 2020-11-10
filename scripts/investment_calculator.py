4bdb8385bf2f1caff405a8719c6d53b2e8507594
Luke Vassor <43808760+lvassor@users.noreply.github.com>
2020-11-10 12:56:05 +0000

    plot fixed, needed to use figure object and put color command at end of plotting code. need to put in a catch for an empty form

--- scripts/investment_calculator.py
+++ scripts/investment_calculator.py
@@ -8,8 +8,10 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from matplotlib.colors import LinearSegmentedColormap
+from matplotlib.figure import Figure
 from io import StringIO, BytesIO
 import base64
+from PIL import Image
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mysecretkey'
@@ -56,7 +58,7 @@ def index():
         return redirect(url_for("prediction"))
     return render_template('home.html', form=form)
 
-@app.route('/prediction')#, methods=['POST'])
+@app.route('/prediction')
 def prediction():
     content = {}
 
@@ -76,24 +78,19 @@ def prediction():
             content['mon_amount'] = int(j)
             payment_matrix.loc[i,j] = simulate(content)
     binary = payment_matrix.applymap(lambda x: 0 if x < content['target'] else 1)
-
-    output = BytesIO()
-    # fig,ax=plt.subplots(figsize=(6,6))
-    f = plt.figure()
+    
+    fig = plt.figure()
     # Define colors
-    colors = ((0.016, 0.102, 0.196), (0.298, 0.686, 0.314))
+    colors = ((223/255, 221/255, 221/255), (0.298, 0.686, 0.314))
     cmap = LinearSegmentedColormap.from_list('Custom', colors, len(colors))
-    ax = sns.heatmap(binary,linewidths=0.3, linecolor='#DFDDDD', cmap=cmap)
-    
-    
-    sns.set(rc={'figure.facecolor':'#041A32'})
+    ax = sns.heatmap(binary,linewidths=0.3, linecolor='#041A32', cmap=cmap)
     ax.set_xlabel("Monthly deposit", color="white")
     ax.set_ylabel("Principal", color="white")
     sns.despine()
     ax.spines['bottom'].set_linewidth(1)
-    ax.spines['bottom'].set_color('white')
+    ax.spines['bottom'].set_color('#041A32')
     ax.spines['left'].set_linewidth(1)
-    ax.spines['left'].set_color('white')
+    ax.spines['left'].set_color('#041A32')
 
     ax.tick_params(axis='y', which='both', colors='white', width=5)
     ax.tick_params(axis='x', which='both', colors='white', width=5)
@@ -103,19 +100,21 @@ def prediction():
     colorbar.set_ticks([0.25,0.75])
     colorbar.set_ticklabels(['Miss target', 'Meet target'])
     colorbar.ax.tick_params(colors="white")
-    # plt.savefig('myplot.png')
-    canvas = FigureCanvas(f)
-    canvas.print_png(output)
-    plot_data = base64.b64encode(output.getvalue()).decode('ascii')
-    output.seek(0)
-    # img = BytesIO()
-    # fig.savefig(img, format='png')
-    # img.seek(0)
-    # # fig.savefig('my_heatmap.png', format='png')
-    # # img.close()
-    # plot_url = base64.b64encode(img.getvalue())
+    plt.ylim(11,0)
+    plt.tight_layout()
+    fig.patch.set_facecolor('#041A32')
+    # Convert plot to img 
+    # plt.savefig(output, format='png',dpi=100, facecolor='#041A32', bbox_inches='tight')
+    pngImage = BytesIO()
+    FigureCanvas(fig).print_png(pngImage)
+
+    # Encode PNG to base64 string
+    pngImageB64String = "data:image/png;base64,"
+    pngImageB64String += base64.b64encode(pngImage.getvalue()).decode('ascii')
+    pngImage.seek(0)
     
-    return render_template('prediction.html',results=results, url=plot_data)
+    
+    return render_template('prediction.html',results=results, image=pngImageB64String)
 
 if __name__=='__main__':
     app.run(debug=True)
